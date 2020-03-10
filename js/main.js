6807b56337a61a72b5077cf8553ca8a60e0d42a3
Mike Salas <michaelsc@thehangar.cr>
2020-03-10 00:53:38 -0600

    varios avances

--- js/main.js
+++ js/main.js
@@ -1,156 +1,163 @@
-// Hello.
-//
-// This is The Scripts used for ___________ Theme
-//
-//
-
 function main() {
+  (function() {
+    "use strict";
 
-(function () {
-   'use strict';
-
-   /* ==============================================
+    /* ==============================================
   	Testimonial Slider
-  	=============================================== */ 
+  	=============================================== */
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
+    $("a.page-scroll").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
               scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
-    /*====================================
-    Show Menu on Book
-    ======================================*/
-    var first = true;
-    $(window).bind('scroll', function() {
-      //var navHeight = $(window).height() - 100;
-      if ($(window).scrollTop() > 0) {
-          $('.navbar-default').addClass('on');
+            },
+            900
+          );
+          $(".navbar-default").addClass("on");
           $("#tf-home .overlay").fadeOut(1000, function() {
             // Animation complete.
             $("#tf-menu").fadeIn(1000);
             $("#tf-home").slideUp(1000, function() {
               if (first) {
-                $("body").stop().animate({scrollTop:0});
+                $("body")
+                  .stop()
+                  .animate({ scrollTop: 0 });
                 first = false;
               }
             });
           });
-      } else {
-          $('.navbar-default').removeClass('on');
+          return false;
+        }
       }
     });
 
-    $('body').scrollspy({ 
-      target: '.navbar-default',
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    var first = true;
+    $(window).bind("scroll", function() {
+      //var navHeight = $(window).height() - 100;
+      if ($(window).scrollTop() > 0) {
+        $(".navbar-default").addClass("on");
+        $("#tf-home .overlay").fadeOut(1000, function() {
+          // Animation complete.
+          $("#tf-menu").fadeIn(1000);
+          $("#tf-home").slideUp(1000, function() {
+            if (first) {
+              $("body")
+                .stop()
+                .animate({ scrollTop: 0 });
+              first = false;
+            }
+          });
+        });
+      } else {
+        $(".navbar-default").removeClass("on");
+      }
+    });
+
+    $("body").scrollspy({
+      target: ".navbar-default",
       offset: 80
-    })
+    });
 
-  	$(document).ready(function() {
-
-      $('#mycontactform-submit').click(function(){
-        $.post("send.php", $("#mycontactform").serialize(),  function(response) {
-          $('#success').show("fast");
+    $(document).ready(function() {
+      $("#mycontactform-submit").click(function() {
+        $.post("send.php", $("#mycontactform").serialize(), function(response) {
+          $("#success").show("fast");
           if (response.indexOf("Invalid") >= 0) {
-            $('#success').addClass("invalid");
+            $("#success").addClass("invalid");
           } else {
-            $('#success').removeClass("invalid");
+            $("#success").removeClass("invalid");
           }
-          $('#success').html(response);
-          setTimeout( function() {
-            $('#success').hide('slow');
+          $("#success").html(response);
+          setTimeout(function() {
+            $("#success").hide("slow");
           }, 3000);
         });
         return false;
       });
 
-  	  $("#team").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
-  	  });
+      $("#team").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        autoHeight: true,
+        itemsCustom: [
+          [0, 1],
+          [450, 2],
+          [600, 2],
+          [700, 2],
+          [1000, 4],
+          [1200, 4],
+          [1400, 4],
+          [1600, 4]
+        ]
+      });
 
-  	  $("#clients").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
+      $("#clients").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        autoHeight: true,
+        itemsCustom: [
+          [0, 1],
+          [450, 2],
+          [600, 2],
+          [700, 2],
+          [1000, 4],
+          [1200, 5],
+          [1400, 5],
+          [1600, 5]
+        ]
+      });
 
       $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+      });
+    });
 
-  	});
-
-  	/*====================================
+    /*====================================
     Portfolio Isotope Filter
     ======================================*/
     $(window).load(function() {
-        var $container = $('#lightbox');
+      var $container = $("#lightbox");
+      $container.isotope({
+        filter: "*",
+        animationOptions: {
+          duration: 750,
+          easing: "linear",
+          queue: false
+        }
+      });
+      $(".cat a").click(function() {
+        $(".cat .active").removeClass("active");
+        $(this).addClass("active");
+        var selector = $(this).attr("data-filter");
         $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: false
+          }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
+        return false;
+      });
     });
-
-
-
-}());
-
-
+  })();
 }
 main();
